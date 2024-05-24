8fc9b88738619e6916d546c54e184c8640eca4e8
drCathieSo.eth <socathie@users.noreply.github.com>
2024-05-24 17:09:22 +0800

    Update benchmarking scripts

--- bench_hash.sh
+++ bench_hash.sh
@@ -12,7 +12,7 @@ cd circuit_js
 node generate_witness.js circuit.wasm ../../test/input.json ../witness.wtns
 cd ..
 snarkjs groth16 setup circuit.r1cs powersOfTau28_hez_final_22.ptau circuit_0000.zkey
-snarkjs zkey contribute circuit_0000.zkey circuit_final.zkey --name="1st Contributor Name" -v
+snarkjs zkey contribute circuit_0000.zkey circuit_final.zkey --name="1st Contributor Name" -v -e="random"
 snarkjs zkey export verificationkey circuit_final.zkey verification_key.json
 gtime -o ../bench_hash_prove.out -v snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json public.json
-gtime -o ../bench_hash_verify.out -v snarkjs groth16 verify circuit_final.zkey public.json proof.json
+gtime -o ../bench_hash_verify.out -v snarkjs groth16 verify verification_key.json public.json proof.json
--- bench_raw.sh
+++ bench_raw.sh
@@ -10,7 +10,7 @@ cd test_js
 node generate_witness.js test.wasm ../../test/input.json ../witness.wtns
 cd ..
 snarkjs groth16 setup test.r1cs powersOfTau28_hez_final_17.ptau circuit_0000.zkey
-snarkjs zkey contribute circuit_0000.zkey circuit_final.zkey --name="1st Contributor Name" -v
+snarkjs zkey contribute circuit_0000.zkey circuit_final.zkey --name="1st Contributor Name" -v -e="random"
 snarkjs zkey export verificationkey circuit_final.zkey verification_key.json
 gtime -o ../bench_raw_prove.out -v snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json public.json
 gtime -o ../bench_raw_verify.out -v snarkjs groth16 verify verification_key.json public.json proof.json
